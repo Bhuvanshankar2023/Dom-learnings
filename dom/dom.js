b2076f478f52f62d74084ca357564c551706b573
Bhuvanshankar2023 <118837670+Bhuvanshankar2023@users.noreply.github.com>
2022-12-07 07:59:58 +0530

    basics of learning dom

---
+++ dom/dom.js
@@ -0,0 +1,56 @@
+// let update = document.getElementById("box-1");
+// update.innerHTML =  "Hi this is an box";
+
+// const nam = document.getElementsByTagName("h1");
+// nam.classlist.add("name")
+
+
+// let news = document.getElementById("box-1");
+// // news.innerHTML = "im working"
+
+// news.classList.add("main");
+// news.classList.remove("box")
+
+
+// let container = document.getElementsByClassName("container");
+// for(let i = 0; i < 10; i++){
+//     const boxelement = document.createElement("div");
+//     boxelement.classList.add("container");
+//     container[0].append(boxelement);
+// }
+
+
+// let container = document.getElementsByClassName("container");
+// for (let i = 0; i < 10 ; i++) {
+//     let boxele = document.createElement("div");
+//     boxele.classList.add("container");
+//     container[0].append(boxele);
+    
+// }
+
+
+// container.style = ""
+// let boxs = document.getElementById("box-1");
+// boxs.innerHTML = "im working"
+// boxs.style.backgroundColor = "blue"
+
+
+
+// let contain = document.getElementsByClassName("container");
+// let boxel = document.createElement("div");
+// boxel.classList.add("container")
+// boxel.append("contain")
+
+
+//  addeventlisteners
+
+// let boxs = document.getElementById("box-1");
+// // let el = 0;
+// const dyna = ()=>{
+//      boxs.style.backgroundColor = "blue"
+//     // alert("im clicked")
+// }
+// boxs.addEventListener("click", dyna);
+
+
+
